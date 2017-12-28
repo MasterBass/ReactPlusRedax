9db3a4fae494e7616e0b7baf75a7bc8d310320dc
Vasyl Suddya <vasyl.suddya@incredible-web.com>
2017-12-28 09:14:30 +0200

    update database.data.json

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,24 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
-// The Firebase Admin SDK to access the Firebase Realtime Database. 
+// The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 exports.newUser = functions.auth.user().onCreate(event => {
-    const user = event.data; // The Firebase user. 
+    const user = event.data; // The Firebase user.
     const usersRef = admin.database().ref('users');
     let role = 'user';
+    // console.log(user);
 
     return usersRef.once('value', function(snapshot) {
         if(snapshot.numChildren() === 0) {
             role = 'admin';
         }
-    }).then(() => {    
-        usersRef.child(user.uid).set({ 
+    }).then(() => {
+        usersRef.child(user.uid).set({
             email: user.email,
-            role: role        
-        });          
+            role: role
+        });
     });
 });
